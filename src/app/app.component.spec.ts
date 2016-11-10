f9232de0b6888bc6758c728de676e8eb4d8e0dc9
theRealRobG <robertnw5@gmail.com>
2016-11-10 08:38:12 +0000

    first test, just to see how Angular 2 TestBed works

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
-import { ActionBoxComponent, MessageBoxComponent, ProgressBarComponent } from './components'
+import { ActionBoxComponent, MessageBoxComponent, ProgressBarComponent } from './components';
+import { ComputerService, UserService } from './services';
+
+let mockService = {
+    model: {
+        totalLife: 3,
+        remainingLife: 3
+    }
+};
 
 describe('App', () => {
     beforeEach(() => {
@@ -11,6 +19,10 @@ describe('App', () => {
                 ActionBoxComponent,
                 MessageBoxComponent,
                 ProgressBarComponent
+            ],
+            providers: [
+                { provide: ComputerService, useValue: mockService },
+                { provide: UserService, useValue: mockService }
             ]
         });
     });
@@ -19,4 +31,12 @@ describe('App', () => {
         let fixture = TestBed.createComponent(AppComponent);
         expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
     });
+
+    it('should set the service models', () => {
+        let fixture = TestBed.createComponent(AppComponent);
+        expect(fixture.componentInstance.computerModel.totalLife).toBe(3);
+        expect(fixture.componentInstance.computerModel.remainingLife).toBe(3);
+        expect(fixture.componentInstance.userModel.totalLife).toBe(3);
+        expect(fixture.componentInstance.userModel.remainingLife).toBe(3);
+    })
 });
